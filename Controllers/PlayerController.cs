ce96bf0fadf0f3b09a66969f057c47a7e682303b
taegangyun <taegangyun@gmail.com>
2024-12-23 00:39:10 +0900

    UI Manager

--- Controllers/PlayerController.cs
+++ Controllers/PlayerController.cs
@@ -19,7 +19,9 @@ public class PlayerController : MonoBehaviour
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
 
-        //Managers.Resource.Instantiate("UI/UI_Button");
+       
+        Managers.UI.ShowPopupUI<UI_Button>();
+        //Managers.UI.ClosePopupUI();
     }
 
 
