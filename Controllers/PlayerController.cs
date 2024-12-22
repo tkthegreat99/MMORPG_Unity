73ce9b195b48a99e0f3e44b08d8c8707a34d00aa
taegangyun <taegangyun@gmail.com>
2024-12-22 23:44:08 +0900

    UI Event

--- Controllers/PlayerController.cs
+++ Controllers/PlayerController.cs
@@ -18,6 +18,8 @@ public class PlayerController : MonoBehaviour
         Managers.Input.KeyAction += OnKeyboard; //구독 신청
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
+
+        //Managers.Resource.Instantiate("UI/UI_Button");
     }
 
 
--- Managers/InputManager.cs
+++ Managers/InputManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager
 { 
@@ -11,7 +12,8 @@ public class InputManager
     bool _pressed = false;
     public void OnUpdate()
     {
-        
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
 
         if(Input.anyKey && KeyAction != null)
             KeyAction.Invoke(); //delegate »£√‚.
